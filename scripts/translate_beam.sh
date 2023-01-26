51650068d1a7997ede53209913be0081c2b7a93d
Nevena Nikolic <nevena.nikolic@uzh.ch>
2023-01-26 01:52:51 +0100

    Uncomment script

--- scripts/translate_beam.sh
+++ scripts/translate_beam.sh
@@ -11,25 +11,25 @@ translations=$base/translations
 
 echo "vary beam size up to k=25"
 echo "************************************************************************************"
-#for (( K=1; K<=25; K+=3 )); do
-#    python translate_beam.py --data data/$lang/prepared --dicts data/$lang/prepared \
-#                        --checkpoint-path assignments/03/baseline/checkpoints/checkpoint_last.pt \
-#                        --output $translations/translation.beam.$K.$trg --batch-size 25 \
-#                        --max-len 500 --alpha 0.0 --beam-size $K 
-#done
+for (( K=1; K<=25; K+=3 )); do
+    python translate_beam.py --data data/$lang/prepared --dicts data/$lang/prepared \
+                        --checkpoint-path assignments/03/baseline/checkpoints/checkpoint_last.pt \
+                        --output $translations/translation.beam.$K.$trg --batch-size 25 \
+                        --max-len 500 --alpha 0.0 --beam-size $K 
+done
 
-#echo "detokenize translations"
-#echo "************************************************************************************"
-#for f in $translations/*.$trg; do
-#    cat $f | perl moses_scripts/detruecase.perl | perl moses_scripts/detokenizer.perl -q -l $trg > $f.detok
-#    rm $f
-#done
+echo "detokenize translations"
+echo "************************************************************************************"
+for f in $translations/*.$trg; do
+    cat $f | perl moses_scripts/detruecase.perl | perl moses_scripts/detokenizer.perl -q -l $trg > $f.detok
+    rm $f
+done
 
-#echo "calculate BLEU"
-#echo "************************************************************************************"
-#for f in $translations/*.detok; do
-#    sacrebleu data/$lang/raw/test.$trg -i $f
-#done
+echo "calculate BLEU"
+echo "************************************************************************************"
+for f in $translations/*.detok; do
+    sacrebleu data/$lang/raw/test.$trg -i $f
+done
 
 echo "translate with squared regularizer"
 echo "************************************************************************************"
